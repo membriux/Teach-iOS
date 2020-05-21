8360fc3a8310248de182117c90ac7528da37a007
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2020-05-21 14:56:19 -0700

    Updated Flix

---
+++ CompletedProjects/Flix/Flix/Views/PosterCell.swift
@@ -0,0 +1,30 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Memo on 11/15/18.
+//  Copyright © 2018 Membriux. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PosterCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    var movie: Movie! {
+        didSet {
+            if movie.posterUrl != nil {
+                posterImageView.af.setImage(withURL: movie.posterUrl!)
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+}
